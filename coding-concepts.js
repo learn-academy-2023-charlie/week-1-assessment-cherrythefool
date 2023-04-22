3878dde152f57d7d1bc6c00f3e966fd0c418ae8c
cherrythefool <sarahandecherry@gmail.com>
2023-04-22 09:45:25 -0600

    initial commit, not done yet

--- coding-concepts.js
+++ coding-concepts.js
@@ -2,53 +2,58 @@
 
 // Examine the following examples.
 
-// First, predict the outcome based on your understanding of the code.
-// Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
+////// First, predict the outcome based on your understanding of the code.
+////// Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
-// --------------------INSTRUCTOR EXAMPLE: What will this log?
-const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+////// --------------------INSTRUCTOR EXAMPLE: What will this log?
+////const colors = ["tangerine", "magenta", "lilac", "daffodil"]
+////// console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+////// a) Your answer:
+////// b) Verify and explain:
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2023"
-// console.log(cohort.length)
+console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: There are 10 characters in the string "LEARN 2023" which is stored in the variable cohort, so I would expect a log of cohort.length to produce 10 as a number.
+
+// b) Verify and explain: This returned 10, nice!
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The log of greeting[4] will return the character at the index assigned the number 4 for the string stored in the variable greeting, or the 5th character by count since js uses zero-indexing. So it should return the character "o" as a string.
+
+// b) Verify and explain: This did return "o", yay!
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The square brackets around [index] indicate that it will find the indexed character for the preceding item, languages. Since the variable of index stores the value of 1, it will give me the item which is indexed at location 1 in the array languages, which would be "Ruby" as a string.
+
+// b) Verify and explain: We got Ruby!
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+//console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I know for strings the .toUpperCase method converts a string to ALL UPPER CASE. I'm not sure what this would do applied to in an array - I think there's a chance it will either return the array with all items all upper case (since all items in the array are strings), but it might also do nothing applied to an array. I'm going with it returning the array of all items all upper case.
+
+// b) Verify and explain: This returned "TypeError: weekendDays.toUpperCase is not a function", I think because .toUpperCase is a string method and not for arrays
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I think this will tell me the type of data that dataTypes.length returns, which would be a number. 
+
+// b) Verify and explain: Hooray for number!
--- interview-questions.md
+++ interview-questions.md
@@ -1,4 +1,4 @@
-# ASSESSMENT 1: Tech Interview Practice Questions
+<!-- # ASSESSMENT 1: Tech Interview Practice Questions
 
 Answer the following questions.
 
@@ -10,35 +10,35 @@ INSTRUCTOR EXAMPLE: What is a conditional statement?
 
 Your answer:
 
-Researched answer:
+Researched answer: -->
 
 1. What is git? What is the difference between git and Github?
 
-Your answer:
+Your answer: Git is a version control software. It makes it possible to make changes to a software while tracking those changes. Git is the software and github is a website which uses that software for shared, remotely-hosted version control.
 
-Researched answer:
+Researched answer: Git is a local version control software, meaning it's software on a device and tracks changes made to a software. GitHub uses git as part of a remotely-located repository. The benefits of using github are that it is free and open to the public, making software changes easier to share 
 
 2. Which JavaScript operators will return a Boolean value?
 
-Your answer:
+Your answer: JavaScript operators that are relational or equals operators, like , will return a Boolean value.
 
-Researched answer:
+Researched answer: JavaScript comparison operators, like equality, relational, and logical operators, can return a Boolean value.  
 
 1. What is the difference between an array's index and its value?
 
-Your answer:
+Your answer: An array's index is the location assignment while the value is the information stored in the array. So if you stored the colors of the rainbow in order in an array, red would be a value and stored at the index of 0, and violet would be a value stored at the index location of 6.
 
-Researched answer:
+Researched answer: item =value
 
 4. Compare and contrast arrays and strings. Please include ways in which they are the same and ways in which they are different.
 
-Your answer:
+Your answer: Arrays and strings are both data types in javascript. An array contains an ordered group of items of varying values, and can contain values of multiple data types as a collection. A string is also ordered, but contains multiple characters rather than items. While a string can contain any character, the characters will all be part of the data type of string. Both arrays and strings are indexed and can be manipulated into different orders.
 
 Researched answer:
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-Your answer:
+Your answer: Proper pair programming involves 2 people working on the same code, using the roles of driver and navigator. The person who is navigating should be thinking through how to write the code and instructing their partner on what to type and any necessary changes, while the driver should be typing the code. While they may work together on problem solving, only one person (the driver) should be working in the file at a time. 
 
 Researched answer:
 
